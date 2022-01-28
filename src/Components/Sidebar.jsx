8cbe663c47113fb4d5c30d502abf097fd4d09c0a
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-01-28 17:02:11 +0530

    Add Tweet Button

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import HomeIcon from '@material-ui/icons/Home';
 import ExploreIcon from '@material-ui/icons/Explore';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MessagesIcon from '@material-ui/icons/Message';
@@ -9,10 +10,12 @@ import ListsIcon from '@material-ui/icons/List';
 import ProfileIcon from '@material-ui/icons/Person';
 import MoreIcon from '@material-ui/icons/More';
 import SidebarOption from './SidebarOption';
+import {Button} from '@material-ui/core';
 
 export default function Sidebar() {
   return <div>
       <TwitterIcon/>
+      <SidebarOption text="Home" Icon={HomeIcon} active/>
       <SidebarOption text="Explore" Icon={ExploreIcon}/>
       <SidebarOption text="Notifications" Icon={NotificationsIcon}/>
       <SidebarOption text="Messages" Icon={MessagesIcon}/>
@@ -20,6 +23,8 @@ export default function Sidebar() {
       <SidebarOption text="Lists" Icon={ListsIcon}/>
       <SidebarOption text="Profile" Icon={ProfileIcon}/>
       <SidebarOption text="More" Icon={MoreIcon}/>
+
+      <Button className='sidebarbtn-tweet' fullWidth varient="outlined">Tweet</Button>
 
   </div>;
 }
--- src/Components/SidebarOption.jsx
+++ src/Components/SidebarOption.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './SidebarOption.css'
 
-export default function SidebarOption({text, Icon}) {
-  return <div className='sidebarOption'>
+export default function SidebarOption({text, Icon, active}) {
+  return <div className={`sidebarOption ${active&&'sidebarOption-active'}`}>
     <Icon/>
     <h2>{text}</h2>
   </div>;
