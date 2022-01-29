8d6235562b75311e7ba92979bdb8b008f232a4ce
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-01-29 23:52:14 +0530

    Add css file for post component

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -11,7 +11,10 @@ export default function Feed() {
 
         <TweetBox/>
 
-        <Post/>
+        <Post text="Wanted to reveal something from a long time. The year of 2021 was one of craziest years and it brought some good news too. I received my dream intership from Mitacs and now i am excited to share that i have also received an internship offer from Microsoft for '22" username="@hiteshsukhwani6"/>
+        <Post text="Wanted to reveal something from a long time. The year of 2021 was one of craziest years and it brought some good news too. I received my dream intership from Mitacs and now i am excited to share that i have also received an internship offer from Microsoft for '22" username="@hiteshsukhwani6"/>
+        <Post text="Wanted to reveal something from a long time. The year of 2021 was one of craziest years and it brought some good news too. I received my dream intership from Mitacs and now i am excited to share that i have also received an internship offer from Microsoft for '22" username="@hiteshsukhwani6"/>
+        <Post text="Wanted to reveal something from a long time. The year of 2021 was one of craziest years and it brought some good news too. I received my dream intership from Mitacs and now i am excited to share that i have also received an internship offer from Microsoft for '22" username="@hiteshsukhwani6"/>
         
     </div>;
 }
--- src/Components/Widgets.jsx
+++ src/Components/Widgets.jsx
@@ -3,6 +3,6 @@ import './Widgets.css'
 
 export default function Widgets() {
   return <div className='widgets'>
-
+    <h3>Widgets</h3>
   </div>;
 }
