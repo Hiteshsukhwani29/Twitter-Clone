951303b82f371bd20d546d486754ffece7a213d4
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2022-01-31 18:52:45 +0530

    Add Data to Firebase

--- src/Components/TweetBox.jsx
+++ src/Components/TweetBox.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 import './TweetBox.css'
 import {Avatar, Button} from '@material-ui/core';
+import db from '../Firebase';
 
 export default function TweetBox() {
+
+    const [ message, setmessage ] = useState("");
+    const [ imgurl, setimgurl ] = useState("");
+
+    const tweet = (e) => {
+        e.preventDefault();
+        db.collection("Posts").add({
+            name: "Hitesh Sukhwani",
+            avatar: "aaa",
+            verified: true,
+            username: "hiteshsukhwani6",
+            desc: message,
+            image: imgurl
+        });
+    };
+
   return <div className='tweetbox'>
       <form>
           <div className='tweetbox-input'>
               <Avatar src="https://avatars.githubusercontent.com/u/55627571?v=4" />
-              <input placeholder="What's happening?" />
+              <input value={message} placeholder="What's happening?" onChange={(e) => setmessage(e.target.value)} />
           </div>
           <div className='tweetboximgurl-input'>
-              <input placeholder="Image Url (optional)" />
+              <input value={imgurl} placeholder="Image Url (optional)" onChange={(e) => setimgurl(e.target.value)} />
           </div>
-          <Button className='tweetboxbtn-tweet'>Tweet</Button>
+          <Button className='tweetboxbtn-tweet' type="submit" onClick={tweet}>Tweet</Button>
       </form>
   </div>;
 }
